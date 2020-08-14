0dc4825fad10bd1a0fd019c170eb1b2b19e4c6e8
isaac4552 <69467362+isaac4552@users.noreply.github.com>
2020-08-14 21:06:22 +0200

    Add files via upload

---
+++ Visual code first.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+
+
+
+    std::cout << "´Hello from first app my name is zana .\n";
+
+
+    int age;
+    std::cout << "enter your age :";
+    std::cin >> age;
+
+    std::cout << "your age is: " << age << "\n";;
+}
