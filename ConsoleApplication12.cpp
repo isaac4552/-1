05f7cf05bf60ca73311ef32b60d4932fa5d614dd
isaac4552 <69467362+isaac4552@users.noreply.github.com>
2020-08-18 23:53:07 +0200

    Add files via upload

---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+int main()
+{
+
+
+
+
+
+    std::cout << "hi to 2nd programm in this class \n";
+    std::cout << "it will be covering if and else command \n";
+    std::cout << "first type your age and you will get the results \n";
+
+    int age;
+    std::cout << "Type your age here : ";
+    std::cin << age;
+
+    if (age >= 5) {
+    
+        std::cout << "you are welcomed to our class ";
+
+        char name;
+        std::cout << "type your name first to sign in the class : \n";
+        std::cin << name;
+        std::cout << "welcome !" << name << "\n";
+
+    else {
+
+        std::cout << "sorry kid .....\n";
+
+
+
+    }
+    
+    
+    
+    }
+
+    
+
+
+
+
+}
